e942acd3f889979fb004186367f451cf3a0ece71
AjmalKBangash <ak1489007@gmail.com>
2023-11-23 16:56:53 +0430

    sliding cards -app in intial stage

---
+++ src/Components/Departments.jsx
@@ -0,0 +1,213 @@
+const Departments = [
+  {
+    id: "1",
+    name: "CARDIOLOGY",
+    pic: "require('./src/Components/CarouselImages/PillsImages/pill01.png')",
+    description:
+      "Used in men who do not make enough of a natural substance called testosterone ",
+    price: 700,
+    discount: 10,
+    category: "wholeSale",
+  },
+  {
+    id: "2",
+    name: "ENDOCRINOLOGY",
+    pic: "require('./src/Components/CarouselImages/PillsImages/pill02.jpeg')",
+    description:
+      "Relief of symptoms associated with seasonal allergic rhinitis due to allergen",
+    price: 700,
+    discount: 20,
+    category: "wholeSale",
+  },
+  {
+    id: "3",
+    name: "ENT",
+    pic: "require('./src/Components/CarouselImages/PillsImages/pill03.webp')",
+    description:
+      "Relief of symptoms associated with seasonal allergic rhinitis due to allergen",
+    price: 400,
+    discount: 50,
+    category: "wholeSale",
+  },
+  {
+    id: "4",
+    name: "EYE",
+    pic: "require('./src/Components/CarouselImages/PillsImages/pill04.jpg')",
+    description:
+      "Relief of symptoms associated with seasonal allergic rhinitis due to allergen",
+    price: 150,
+    discount: 32,
+    category: "wholeSale",
+  },
+  {
+    id: "5",
+    name: "PHYSICA",
+    pic: "require('./src/Components/CarouselImages/PillsImages/pill05.jpg')",
+    description:
+      "Relief of symptoms associated with seasonal allergic rhinitis due to allergen",
+    price: 650,
+    discount: 20,
+    category: "wholeSale",
+  },
+  {
+    id: "6",
+    name: "GASTROENTEROLOGY",
+    pic: "require('./src/Components/CarouselImages/PillsImages/pill06.png')",
+    description:
+      "24 Hour on call for all the patients from all the KPK as well as patients from abroad e.g. Afghanistan and some part of Punjab. GI Lab is open for patients throughout the week for routine procedures e.g. Upper & Lower GI Endoscopp, Banding, Scierotheraphy and Biopsy. Dilation, Stenting, Polypectomy, Sigmoidoscopy, Colonoscopy and m importantly ERCP etc. Patients are also admitted on daily basis for acute emergency, and for workup. Most of the Medicines are provided free from Hospital on daily basis. Doctors Nursing Staff Paramedics are present round o'clock for patients care.Gastroenterology Department consist of 35 beds but due to heavy flow of patients extra beds a given to patients which increases number of patients to fifty d sixty routinely. Patients Suffering from 3 and Hepatitis B are properly counseled about their disease and also help them how to get their treatment free from Pakistan Bait-Ui-Mall. Most of patients investigations are done in hospital laboratory free of cost. Gastroenterology Department has highly qualified consultant.24 Hour on call for all the patients from all the KPK as well as patients from abroad e.g. Afghanistan and some part of Punjab. GI Lab is open for patients throughout the week for routine procedures e.g. Upper & Lower GI Endoscopp, Banding, Scierotheraphy and Biopsy. Dilation, Stenting, Polypectomy, Sigmoidoscopy, Colonoscopy and m importantly ERCP etc. Patients are also admitted on daily basis for acute emergency, and for workup. Most of the Medicines are provided free from Hospital on daily basis. Doctors Nursing Staff Paramedics are present round o'clock for patients care.Gastroenterology Department consist of 35 beds but due to heavy flow of patients extra beds a given to patients which increases number of patients to fifty d sixty routinely. Patients Suffering from 3 and Hepatitis B are properly counseled about their disease and also help them how to get their treatment free from Pakistan Bait-Ui-Mall. Most of patients investigations are done in hospital laboratory free of cost. Gastroenterology Department has highly qualified consultant.24 Hour on call for all the patients from all the KPK as well as patients from abroad e.g. Afghanistan and some part of Punjab. GI Lab is open for patients throughout the week for routine procedures e.g. Upper & Lower GI Endoscopp, Banding, Scierotheraphy and Biopsy. Dilation, Stenting, Polypectomy, Sigmoidoscopy, Colonoscopy and m importantly ERCP etc. Patients are also admitted on daily basis for acute emergency, and for workup. Most of the Medicines are provided free from Hospital on daily basis. Doctors Nursing Staff Paramedics are present round o'clock for patients care.Gastroenterology Department consist of 35 beds but due to heavy flow of patients extra beds a given to patients which increases number of patients to fifty d sixty routinely. Patients Suffering from 3 and Hepatitis B are properly counseled about their disease and also help them how to get their treatment free from Pakistan Bait-Ui-Mall. Most of patients investigations are done in hospital laboratory free of cost. Gastroenterology Department has highly qualified consultant.24 Hour on call for all the patients from all the KPK as well as patients from abroad e.g. Afghanistan and some part of Punjab. GI Lab is open for patients throughout the week for routine procedures e.g. Upper & Lower GI Endoscopp, Banding, Scierotheraphy and Biopsy. Dilation, Stenting, Polypectomy, Sigmoidoscopy, Colonoscopy and m importantly ERCP etc. Patients are also admitted on daily basis for acute emergency, and for workup. Most of the Medicines are provided free from Hospital on daily basis. Doctors Nursing Staff Paramedics are present round o'clock for patients care.Gastroenterology Department consist of 35 beds but due to heavy flow of patients extra beds a given to patients which increases number of patients to fifty d sixty routinely. Patients Suffering from 3 and Hepatitis B are properly counseled about their disease and also help them how to get their treatment free from Pakistan Bait-Ui-Mall. Most of patients investigations are done in hospital laboratory free of cost. Gastroenterology Department has highly qualified consultant.24 Hour on call for all the patients from all the KPK as well as patients from abroad e.g. Afghanistan and some part of Punjab. GI Lab is open for patients throughout the week for routine procedures e.g. Upper & Lower GI Endoscopp, Banding, Scierotheraphy and Biopsy. Dilation, Stenting, Polypectomy, Sigmoidoscopy, Colonoscopy and m importantly ERCP etc. Patients are also admitted on daily basis for acute emergency, and for workup. Most of the Medicines are provided free from Hospital on daily basis. Doctors Nursing Staff Paramedics are present round o'clock for patients care.Gastroenterology Department consist of 35 beds but due to heavy flow of patients extra beds a given to patients which increases number of patients to fifty d sixty routinely. Patients Suffering from 3 and Hepatitis B are properly counseled about their disease and also help them how to get their treatment free from Pakistan Bait-Ui-Mall. Most of patients investigations are done in hospital laboratory free of cost. Gastroenterology Department has highly qualified consultant.",
+    price: 30,
+    discount: 20,
+    category: "wholeSale",
+  },
+  {
+    id: "7",
+    name: "GYNAE",
+    pic: "require('./src/Components/CarouselImages/PillsImages/pill07.png')",
+    description:
+      "Relief of symptoms associated with seasonal allergic rhinitis due to allergen",
+    price: 90,
+    discount: 20,
+    category: "wholeSale",
+  },
+  {
+    id: "8",
+    name: "    MAXILLOFACIAL AND DENTISTRY",
+    pic: "require('./src/Components/CarouselImages/PillsImages/pill08.jpg')",
+    description:
+      "Relief of symptoms associated with seasonal allergic rhinitis due to allergen",
+    price: 700,
+    discount: 20,
+    category: "wholeSale",
+  },
+  {
+    id: "9",
+    name: "    DEPTT OF ORTHOPEDIC & SPINE SURGERY",
+    pic: "require('./src/Components/CarouselImages/PillsImages/pill01.png')",
+    description:
+      "Used in men who do not make enough of a natural substance called testosterone ",
+    price: 700,
+    discount: 10,
+    category: "wholeSale",
+  },
+  {
+    id: "10",
+    name: "PATHOLOGY",
+    pic: "require('./src/Components/CarouselImages/PillsImages/pill02.jpeg')",
+    description:
+      "Relief of symptoms associated with seasonal allergic rhinitis due to allergen",
+    price: 700,
+    discount: 20,
+    category: "wholeSale",
+  },
+  {
+    id: "11",
+    name: "PEADS",
+    pic: "require('./src/Components/CarouselImages/PillsImages/pill03.webp')",
+    description:
+      "Relief of symptoms associated with seasonal allergic rhinitis due to allergen",
+    price: 400,
+    discount: 50,
+    category: "wholeSale",
+  },
+  {
+    id: "12",
+    name: "    PLASTIC SURGERY",
+    pic: "require('./src/Components/CarouselImages/PillsImages/pill04.jpg')",
+    description:
+      "Relief of symptoms associated with seasonal allergic rhinitis due to allergen",
+    price: 150,
+    discount: 32,
+    category: "wholeSale",
+  },
+  {
+    id: "13",
+    name: "RADIOLOGY",
+    pic: "require('./src/Components/CarouselImages/PillsImages/pill05.jpg')",
+    description:
+      "Relief of symptoms associated with seasonal allergic rhinitis due to allergen",
+    price: 650,
+    discount: 20,
+    category: "wholeSale",
+  },
+  {
+    id: "14",
+    name: "SURGICAL",
+    pic: "require('./src/Components/CarouselImages/PillsImages/pill06.png')",
+    description:
+      "Lalo roghlo za patal londe pategm aw za gadeegam jar shamma bachpana",
+    price: 30,
+    discount: 20,
+    category: "wholeSale",
+  },
+  {
+    id: "15",
+    name: "    ACCIDENT AND EMERGENCY",
+    pic: "require('./src/Components/CarouselImages/PillsImages/pill07.png')",
+    description:
+      "Relief of symptoms associated with seasonal allergic rhinitis due to allergen",
+    price: 90,
+    discount: 20,
+    category: "wholeSale",
+  },
+  {
+    id: "16",
+    name: "    HUMAN RESOURCEY",
+    pic: "require('./src/Components/CarouselImages/PillsImages/pill08.jpg')",
+    description:
+      "Relief of symptoms associated with seasonal allergic rhinitis due to allergen",
+    price: 700,
+    discount: 20,
+    category: "wholeSale",
+  },
+  {
+    id: "17",
+    name: " ONCOLOGY",
+    pic: "require('./src/Components/CarouselImages/PillsImages/pill08.jpg')",
+    description:
+      "Relief of symptoms associated with seasonal allergic rhinitis due to allergen",
+    price: 700,
+    discount: 20,
+    category: "wholeSale",
+  },
+  {
+    id: "18",
+    name: "       IT DEPARTMENT",
+    pic: "require('./src/Components/CarouselImages/PillsImages/pill01.png')",
+    description:
+      "Used in men who do not make enough of a natural substance called testosterone ",
+    price: 700,
+    discount: 10,
+    category: "wholeSale",
+  },
+  {
+    id: "19",
+    name: "    BIOMEDICAL ENGINEERING",
+    pic: "require('./src/Components/CarouselImages/PillsImages/pill02.jpeg')",
+    description:
+      "Relief of symptoms associated with seasonal allergic rhinitis due to allergen",
+    price: 700,
+    discount: 20,
+    category: "wholeSale",
+  },
+  {
+    id: "20",
+    name: "    PURCHASE AND PROCUREMENT",
+    pic: "require('./src/Components/CarouselImages/PillsImages/pill03.webp')",
+    description:
+      "Relief of symptoms associated with seasonal allergic rhinitis due to allergen",
+    price: 400,
+    discount: 50,
+    category: "wholeSale",
+  },
+  {
+    id: "21",
+    name: "        DEPARTMENT OF ANESTHESIA AND PAIN MANAGEMENT",
+    pic: "require('./src/Components/CarouselImages/PillsImages/pill04.jpg')",
+    description:
+      "Relief of symptoms associated with seasonal allergic rhinitis due to allergen",
+    price: 150,
+    discount: 32,
+    category: "wholeSale",
+  },
+];
+export default Departments;
---
+++ src/Components/SlidingCard.js
@@ -0,0 +1,113 @@
+import React, { useState } from "react";
+import "./SlidingCard.css";
+// import Container from "@mui/material/Container";
+import { Container, Grid } from "@mui/material";
+import Departments from "./Departments";
+import femaleprofile from "/home/ajay/Desktop/django-react/sliding-cards-app/src/Media/hijab.jpg";
+import maleprofile from "/home/ajay/Desktop/django-react/sliding-cards-app/src/Media/male-profile.webp";
+// import
+
+// REACT ICONS
+import { AiOutlineCaretLeft, AiFillCaretRight } from "react-icons/ai";
+
+const SlidingCard = () => {
+  const [position, setPosition] = useState(0);
+
+  const moveLeft = () => {
+    setPosition(position - 1);
+  };
+
+  const moveRight = () => {
+    setPosition(position + 1);
+  };
+  console.log(Departments.length);
+  return (
+    // <Container>
+    <Grid container style={{ border: "1px solid purple" }}>
+      <Grid className="top-sliding-container" xs={12} sm={10} md={10} lg={10}>
+        {/* <div className="sliding-card-container"> */}
+        <Grid className="sliding-card-container" xs={10} sm={10} lg={10}>
+          <div
+            className="sliding-card"
+            style={{ transform: `translateX(${position * 236}px)` }}
+          >
+            {Departments.map((department, id) => {
+              console.log(position);
+              return (
+                <Grid
+                  className="card"
+                  item
+                  lg={6}
+                  md={6}
+                  xs={6}
+                  spacing={2}
+                  style={{ border: "1px solid purple" }}
+                >
+                  {/* <div className="card"> */}
+                  <div className="card-portion-01">
+                    {/* <div className="card-portion-01-dp"> */}
+                    <img className="card-portion-01-dp" src={femaleprofile} />
+                    {/* </div> */}
+                  </div>
+                  <div className="card-portion-02">
+                    <div>LAILA GULALAI</div>
+                    <p>
+                      I am fully satidfied with his work and i am also
+                      recommending his work to all of you her is my profile
+                    </p>
+                    <button>View More</button>
+                  </div>
+                  <div className="card-portion-angle"></div>
+                  {department.name}
+                  {/*  </div> */}
+                </Grid>
+              );
+            })}
+          </div>
+        </Grid>
+        {/* </div> */}
+        <div className="controls">
+          <button
+            className="btn01"
+            onClick={moveLeft}
+            disabled={position === -10}
+          >
+            <AiOutlineCaretLeft
+              style={{ fontSize: "30px", color: "#045e5e" }}
+            />
+          </button>
+          <button
+            className="btn02"
+            onClick={moveRight}
+            disabled={position === 0}
+          >
+            <AiFillCaretRight style={{ fontSize: "30px", color: "#045e5e" }} />
+          </button>
+        </div>
+      </Grid>
+    </Grid>
+    // </Container>
+  );
+};
+
+export default SlidingCard;
+
+{
+  /* Your card content goes here */
+}
+{
+  /* <div className="card">Card Content0</div>
+          <div className="card">Card Content1</div>
+          <div className="card">Card Content2</div>
+          <div className="card">Card Content03</div>
+          <div className="card">Card Content3</div>
+          <div className="card">Card Content4</div>
+          <div className="card">Card Content5</div>
+          <div className="card">Card Content6</div>
+          <div className="card">Card Content7</div>
+          <div className="card">Card Content8</div>
+          <div className="card">Card Content9</div> */
+}
+{
+  /* Add more cards as needed */
+}
